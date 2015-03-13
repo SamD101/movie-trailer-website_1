3dd13b518b29b0efc56eb07ea89bb211963e86b8
SamD101 <syaamantakd.web@gmail.com>
2015-03-13 10:51:12 +0530

    Committed by Udacity

---
+++ UND-FSWD-Project 1/media.py
@@ -0,0 +1,20 @@
+# This file contains Movie class definition.
+
+
+
+class Movie(object):
+    '''Class to hold my favorite movies.
+    Attributes are:
+    title: A string containing the movie title.
+    storyline: A string containing a short description of the movie.
+    poster_url: A string with URL for the movie poster image file.
+    trailer_url: A string with URL for the official YouTube trailer.
+    '''
+    def __init__(self, title, storyline, poster_url, trailer_url):
+        '''
+        Initializes Movie class with title, storyline, poster_url, trailer_url.
+        '''
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_url
+        self.trailer_youtube_url = trailer_url
